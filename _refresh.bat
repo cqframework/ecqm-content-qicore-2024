629a9642da2e94b288727a5971bfe31b08ca3d59
Bryn Rhodes <bryn@alphora.com>
2026-03-12 20:28:35 -0600

    Refreshed bundles with more recent tooling and excluded patient test data (so they are just the measures, libraries, and value sets)

--- _refresh.bat
+++ _refresh.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET tooling_jar=tooling-cli-3.0.0.jar
+SET tooling_jar=tooling-cli-3.8.0.jar
 SET input_cache_path=%~dp0input-cache
 SET ig_ini_path=%~dp0ig.ini
 
@@ -11,18 +11,18 @@ GOTO igpublish
 
 :isonline
 ECHO We're online, setting publish to local sandbox FHIR server
-SET fsoption=-fs=https://cqf-ruler.ecqm.icfcloud.com/fhir
+SET fsoption=
 
 :igpublish
 
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d -p %fsoption%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d -p %fsoption%
+	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d %fsoption%
+	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d %fsoption%
 ) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d -p %fsoption%
-	JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d -p %fsoption%
+	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d %fsoption%
+	JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d %fsoption%
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 )
--- _updateCQFTooling.sh
+++ _updateCQFTooling.sh
@@ -2,17 +2,17 @@
 #DO NOT EDIT WITH WINDOWS
 #exit 1
 
-r=snapshots
+r=releases
 g=org.opencds.cqf
 a=tooling-cli
-v=3.0.0-SNAPSHOT
+v=3.8.0
 
 dlurl='https://oss.sonatype.org/service/local/artifact/maven/redirect?r='${r}'&g='${g}'&a='${a}'&v='${v}''
 
 echo ${dlurl}
 
 input_cache_path=./input-cache/
-tooling_jar=tooling-cli-3.0.0.jar
+tooling_jar=tooling-cli-3.8.0.jar
 
 set -e
 if ! type "curl" > /dev/null; then
